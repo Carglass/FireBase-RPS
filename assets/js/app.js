0ccdea1d2ddc1af2d38ee764dcfb40b4a2748def
Maxime Burlat <maximeburlat@gmail.com>
2018-03-28 18:19:43 -0500

    can join a session, now need to focus on conditional display before entering game management phase

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,7 +29,7 @@ database.ref('sessions').on("child_added", function(snapshot){
         $('#current-game').text('Waiting for a worthy opponent');
     } else {
         let sessionName = $('<div>' + snapshot.val().creator.displayName + ' session</div>');
-        let joinButton = $('<button>Join Game</button>');
+        let joinButton = $('<button></button>').attr('id', snapshot.key).text('join Session').addClass('join-button');
         $('#open-sessions').append(sessionName).append(joinButton);
     }
 });
@@ -72,7 +72,7 @@ $(document).ready(function(){
     });
 
     // create session button
-    $(document).on('click','#session-control', function(event){
+    $(document).on('click','#create-game', function(event){
         let session = {
             'creator': {
                 'displayName': firebase.auth().currentUser.displayName,
@@ -81,6 +81,16 @@ $(document).ready(function(){
             state: STATE.OPEN,
         }
         database.ref('sessions').push(session);
+    });
+
+    $(document).on('click','.join-button', function(event){
+        let sessionUid = $(event.target).attr('id');
+        let joiner = {
+            'displayName': firebase.auth().currentUser.displayName,
+            'uid': firebase.auth().currentUser.uid,
+        };
+        database.ref('sessions/' + sessionUid + '/joiner').set(joiner);
+        database.ref('sessions/' + sessionUid + '/state').set(STATE.CLOSED);
     });
 
 });
